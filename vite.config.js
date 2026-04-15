f2ccfd9f5a8434d88e4e11e4b96c52bf5c11f0b5
Jason Griffin <griffin34@gmail.com>
2026-04-14 19:19:04 -0700

    fix: add emptyOutDir to vite config, broaden artifact glob, add build dir listing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: 'src/renderer',
-  build: { outDir: '../../dist/renderer' },
+  build: { outDir: '../../dist/renderer', emptyOutDir: true },
   server: { port: 5173 }
 })
